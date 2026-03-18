dfa2501af6854825e18fe501f6454cb6e8dcde7a
Informationen-4B <mazimazi@outlook.de>
2026-03-18 22:52:36 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,12 @@
 // Produktions-Konfiguration für Informationen 4B.
-window.CLASSPLANNER_CONFIG = {
-  firebaseConfig: {
-    apiKey: "AIzaSyCq0CpAKCMqKP30EEYZ3HxFNBHgjD7P4Ro",
-    authDomain: "informationen-4b.firebaseapp.com",
-    projectId: "informationen-4b",
-    storageBucket: "informationen-4b.firebasestorage.app",
-    messagingSenderId: "443143023390",
-    appId: "1:443143023390:web:270bd0a36fff92991fb70f",
-  },
+const firebaseConfig = {
+  apiKey: "AIzaSyCq0CpAKCMqKP30EEYZ3HxFNBHgjD7P4Ro",
+  authDomain: "informationen-4b.firebaseapp.com",
+  projectId: "informationen-4b",
+  storageBucket: "informationen-4b.firebasestorage.app",
+  messagingSenderId: "443143023390",
+  appId: "1:443143023390:web:270bd0a36fff92991fb70f"
+};
   // Bitte auf die echten Rollen-Mails anpassen:
   rootAdminEmail: "Matthias.HANDL@outlook.com",
   classRep1Email: "klassensprecherin1@example.com",
