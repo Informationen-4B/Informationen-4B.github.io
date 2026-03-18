1c078a360c63d69476de4b0ab699af3929d1e054
Informationen-4B <mazimazi@outlook.de>
2026-03-18 22:57:56 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,23 @@
 // Produktions-Konfiguration für Informationen 4B.
-const firebaseConfig = {
-  apiKey: "AIzaSyCq0CpAKCMqKP30EEYZ3HxFNBHgjD7P4Ro",
-  authDomain: "informationen-4b.firebaseapp.com",
-  projectId: "informationen-4b",
-  storageBucket: "informationen-4b.firebasestorage.app",
-  messagingSenderId: "443143023390",
-  appId: "1:443143023390:web:270bd0a36fff92991fb70f"
-};
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCq0CpAKCMqKP30EEYZ3HxFNBHgjD7P4Ro",
+    authDomain: "informationen-4b.firebaseapp.com",
+    projectId: "informationen-4b",
+    storageBucket: "informationen-4b.firebasestorage.app",
+    messagingSenderId: "443143023390",
+    appId: "1:443143023390:web:aae3b9bc5e0d6d331fb70f"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
   // Bitte auf die echten Rollen-Mails anpassen:
   rootAdminEmail: "Matthias.HANDL@outlook.com",
   classRep1Email: "klassensprecherin1@example.com",
