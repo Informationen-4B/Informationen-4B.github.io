b800b6ab842a33b22407855b19abee7bc4bc6658
Informationen-4B <mazimazi@outlook.de>
2026-03-18 22:21:26 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// Datei mit echten Firebase-Werten befüllen.
+// Diese Datei wird im Browser geladen und muss daher bewusst öffentliche Web-App-Werte enthalten.
+window.CLASSPLANNER_CONFIG = {
+  firebaseConfig: {
+    apiKey: "REPLACE_ME",
+    authDomain: "REPLACE_ME",
+    projectId: "REPLACE_ME",
+    storageBucket: "REPLACE_ME",
+    messagingSenderId: "REPLACE_ME",
+    appId: "REPLACE_ME",
+  },
+  rootAdminEmail: "dein.name@example.com",
+  classRep1Email: "klassensprecherin1@example.com",
+  classRep2Email: "klassensprecherin2@example.com",
+  deputyRepEmail: "stellvertreter@example.com",
+};
