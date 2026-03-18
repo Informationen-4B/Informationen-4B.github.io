a8e22287ef21472990b64a1b5d24e6a0864f979a
Informationen-4B <mazimazi@outlook.de>
2026-03-18 22:59:32 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,11 @@
-// Produktions-Konfiguration für Informationen 4B.
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCq0CpAKCMqKP30EEYZ3HxFNBHgjD7P4Ro",
-    authDomain: "informationen-4b.firebaseapp.com",
-    projectId: "informationen-4b",
-    storageBucket: "informationen-4b.firebasestorage.app",
-    messagingSenderId: "443143023390",
-    appId: "1:443143023390:web:aae3b9bc5e0d6d331fb70f"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCq0CpAKCMqKP30EEYZ3HxFNBHgjD7P4Ro",
+  authDomain: "informationen-4b.firebaseapp.com",
+  projectId: "informationen-4b",
+  storageBucket: "informationen-4b.firebasestorage.app",
+  messagingSenderId: "443143023390",
+  appId: "1:443143023390:web:aae3b9bc5e0d6d331fb70f"
+};
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
