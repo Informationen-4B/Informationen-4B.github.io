28194599cdd8fb4090cec468239a176967845268
Informationen-4B <mazimazi@outlook.de>
2026-03-18 22:37:55 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,20 @@
-// Datei mit echten Firebase-Werten befüllen.
-// Diese Datei wird im Browser geladen und muss daher bewusst öffentliche Web-App-Werte enthalten.
-window.CLASSPLANNER_CONFIG = {
-  firebaseConfig: {
-    apiKey: "AIzaSyCq0CpAKCMqKP30EEYZ3HxFNBHgjD7P4Ro",
-    authDomain: "informationen-4b.firebaseapp.com",
-    projectId: "informationen-4b",
-    storageBucket: "informationen-4b.firebasestorage.app",
-    messagingSenderId: "443143023390",
-    appId: "1:443143023390:web:270bd0a36fff92991fb70f",
-  },
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCq0CpAKCMqKP30EEYZ3HxFNBHgjD7P4Ro",
+  authDomain: "informationen-4b.firebaseapp.com",
+  projectId: "informationen-4b",
+  storageBucket: "informationen-4b.firebasestorage.app",
+  messagingSenderId: "443143023390",
+  appId: "1:443143023390:web:270bd0a36fff92991fb70f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
   rootAdminEmail: "dein.name@example.com",
   classRep1Email: "klassensprecherin1@example.com",
   classRep2Email: "klassensprecherin2@example.com",
