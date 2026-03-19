c4d308e9ce013f42c40a7c90857d2a29bb9f7479
Informationen-4B <mazimazi@outlook.de>
2026-03-19 21:01:32 +0100

    Update register.js

--- register.js
+++ register.js
@@ -5,15 +5,21 @@ import { setDoc, doc } from "https://www.gstatic.com/firebasejs/10.14.1/firebase
 window.register = async () => {
   try{
     const user = await createUserWithEmailAndPassword(auth,email.value,password.value);
-    await updateProfile(user.user,{displayName:name.value});
+
+    await updateProfile(user.user,{
+      displayName:name.value
+    });
+
     await setDoc(doc(db,"users",user.user.uid),{
       name:name.value,
       email:email.value,
       role:"user",
       status:"pending"
     });
-    error.innerText = "Warte auf Freigabe";
+
+    error.innerText="Warte auf Freigabe";
+
   }catch(e){
-    error.innerText = e.code;
+    error.innerText=e.code;
   }
 }
