2d9f431a17c92315a2324506988814e5ad793c72
Informationen-4B <mazimazi@outlook.de>
2026-03-19 20:56:10 +0100

    Create login.js

---
+++ login.js
@@ -0,0 +1,11 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+
+window.login = async () => {
+  try{
+    await signInWithEmailAndPassword(auth, email.value, password.value);
+    window.location.href = "dashboard.html";
+  }catch(e){
+    error.innerText = e.code;
+  }
+}
